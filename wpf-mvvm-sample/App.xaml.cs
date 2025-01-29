af59cce22420e070b23d2f74681017576a974421
ino <87593133+inoUwU@users.noreply.github.com>
2025-01-30 00:40:09 +0900

    Init Project

---
+++ wpf-mvvm-sample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_mvvm_sample;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
