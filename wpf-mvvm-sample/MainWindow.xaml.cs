490da402e46161e468a9d9c66d0a3a937eade0e1
ino <87593133+inoUwU@users.noreply.github.com>
2025-01-30 00:43:52 +0900

    DataGridの特定セルへのフォーカスをViewModelから行うサンプルを実装

--- wpf-mvvm-sample/MainWindow.xaml.cs
+++ wpf-mvvm-sample/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CommunityToolkit.Mvvm.Messaging;
+using wpf_mvvm_sample.ViewModels;
 
 namespace wpf_mvvm_sample;
 
@@ -19,5 +13,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        // メッセージの受信登録
+        WeakReferenceMessenger.Default.Register<MainWindowVm.FocusRequestMessage>(this, (r, m) =>
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (MainDataGrid.Items.Count > m.RowIndex &&
+                    MainDataGrid.Columns.Count > m.ColumnIndex)
+                {
+                    // セルを選択
+                    MainDataGrid.SelectedItem = MainDataGrid.Items[m.RowIndex];
+
+                    // フォーカスを設定
+                    var cell = new DataGridCellInfo(
+                        MainDataGrid.Items[m.RowIndex],
+                        MainDataGrid.Columns[m.ColumnIndex]
+                    );
+                    MainDataGrid.CurrentCell = cell;
+                    MainDataGrid.Focus();
+
+                    // セルを編集モードにする場合は以下を追加
+                    MainDataGrid.BeginEdit();
+                }
+            }));
+        });
     }
 }
---
+++ wpf-mvvm-sample/ViewModels/MainWindowVm.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace wpf_mvvm_sample.ViewModels;
+
+// Models
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public partial class MainWindowVm : ObservableValidator
+{
+    public MainWindowVm()
+    {
+        People = new ObservableCollection<Person>
+        {
+            new Person { Name = "山田太郎", Age = 30 },
+            new Person { Name = "鈴木花子", Age = 25 }
+        };
+    }
+
+    [ObservableProperty] private ObservableCollection<Person> _people;
+
+    // DataGridのフォーカス制御用のメッセージ
+    public class FocusRequestMessage
+    {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
+    }
+
+    [RelayCommand]
+    private void SetFocus()
+    {
+        if (People.Any())
+        {
+            // 1行目のNameカラムにフォーカスを設定
+            // CurrentCell = new DataGridCellInfo(People[0], DataGridColumn.CanUserReorderProperty);
+            WeakReferenceMessenger.Default.Send(new FocusRequestMessage
+            {
+                RowIndex = 0,
+                ColumnIndex = 0
+            });
+        }
+    }
+}
