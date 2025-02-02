6817f63c7a0c2154d41524cbcd81517d541c5065
ino <87593133+inoUwU@users.noreply.github.com>
2025-02-03 00:44:44 +0900

    バリデーション方法の追加

---
+++ wpf-mvvm-sample/ValidationServices/InputValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace wpf_mvvm_sample.ValidationServices;
+
+public class InputValidation
+{
+    // ここに実際のバリデーションロジックを記述します
+    // 例えば、名前が特定のパターンに一致するかどうかをチェックするなど
+    public bool NameValidate(string name)
+    {
+        return !string.IsNullOrWhiteSpace(name);
+    }
+}
--- wpf-mvvm-sample/ViewModels/MainWindowVm.cs
+++ wpf-mvvm-sample/ViewModels/MainWindowVm.cs
@@ -1,30 +1,71 @@
 ﻿using System.Collections.ObjectModel;
-using System.Windows.Controls;
+using System.ComponentModel.DataAnnotations;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using wpf_mvvm_sample.ValidationServices;
 
 namespace wpf_mvvm_sample.ViewModels;
 
 // Models
-public class Person
+public partial class Person : ObservableValidator
 {
-    public string Name { get; set; }
-    public int Age { get; set; }
+    private readonly InputValidation _service;
+
+    public Person(InputValidation service)
+    {
+        this._service = service;
+    }
+
+    [ObservableProperty]
+    [CustomValidation(typeof(Person), nameof(ValidateName))]
+    private string? _name;
+
+    [ObservableProperty]
+    [CustomValidation(typeof(Person), nameof(ValidateAge))]
+    private int _age;
+
+    /// <summary>
+    /// 名前のバリデーション
+    /// </summary>
+    public static ValidationResult? ValidateName(string name, ValidationContext context)
+    {
+        // 共通化されたバリデーションロジックを呼び出す
+        var instance = (Person)context.ObjectInstance;
+        var isValid = instance._service.NameValidate(name);
+        return isValid ? ValidationResult.Success : new ValidationResult("The name was not validated by the fancy service");
+    }
+
+    /// <summary>
+    /// 年齢のバリデーション
+    /// </summary>
+    public static ValidationResult? ValidateAge(int age, ValidationContext context)
+    {
+        if (age <= 0 || age > 120)
+        {
+            return new ValidationResult("Age must be a positive number between 1 and 120.");
+        }
+
+        return ValidationResult.Success;
+    }
 }
 
 public partial class MainWindowVm : ObservableValidator
 {
     public MainWindowVm()
     {
+       var service = new InputValidation(); 
+        
         People = new ObservableCollection<Person>
         {
-            new Person { Name = "山田太郎", Age = 30 },
-            new Person { Name = "鈴木花子", Age = 25 }
+            new Person(service) { Name = "山田太郎", Age = 30 },
+            new Person(service) { Name = "鈴木花子", Age = 25 }
         };
     }
 
     [ObservableProperty] private ObservableCollection<Person> _people;
+
+    [ObservableProperty] private DateTime? _selectedYearMonth;
 
     // DataGridのフォーカス制御用のメッセージ
     public class FocusRequestMessage
